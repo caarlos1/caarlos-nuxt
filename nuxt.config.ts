832e8bec67fdf4a56f30851c1ab5ab5f09c2dd02
Carlos Roberto <crpu4a@gmail.com>
2022-06-01 12:39:05 -0300

    Cofinguração do projeto - Criação da página index - Arquivo prettierrc - Storybook

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,9 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    typescript: {
-      shim: false
-    }
-  })
-  
+  typescript: {
+    shim: false,
+  },
+  storybook: {},
+});
