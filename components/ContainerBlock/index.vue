a62d1f35aab0573c93fee5d5e72f3375bbfe6872
Carlos Roberto <crpu4a@gmail.com>
2022-06-11 15:21:49 -0300

    Create ContainerBlock component.

---
+++ components/ContainerBlock/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+export interface ContainerBlockProps {
+  bg?: string;
+}
+
+withDefaults(defineProps<ContainerBlockProps>(), {
+  bg: "",
+});
+</script>
+
+<template>
+  <div class="block__container" :class="{ ['custom--background ' + bg]: bg }">
+    <div class="block__slot">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "./style.scss";
+</style>
