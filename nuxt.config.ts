43386aa195140b6b0e45cc9add08729309283a83
Carlos Roberto <crpu4a@gmail.com>
2022-06-11 15:18:08 -0300

    Modify sheets assets directory

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,11 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  css: ["@/assets/sheets/reset.scss", "@/assets/sheets/fonts.scss", "@/assets/sheets/global.scss"],
   typescript: {
     shim: false,
   },
-  css: ["@/assets/fonts.css", "@/assets/reset.css", "@/assets/global.css"],
+  storybook: {
+    // No suport for Nuxt 3
+  }
 });
