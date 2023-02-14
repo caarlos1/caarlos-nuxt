6925bd9b4d7cf4689a32ecde89623ad46da702b2
Carlos Roberto <carlos.junior@dotgroup.com.br>
2023-02-14 12:33:47 -0300

    add tests to ContainerHeader

--- components/ContainerHeader/ContainerHeader.unit.spec.ts
+++ components/ContainerHeader/ContainerHeader.unit.spec.ts
@@ -7,4 +7,20 @@ describe("ContainerHeader - unit", () => {
     const wrapper = mount(ContainerHeader);
     expect(wrapper.vm).toBeDefined();
   });
+
+  it("should render a default title with h2 when not provide props", () => {
+    const wrapper = mount(ContainerHeader);
+    expect(wrapper.text()).toContain("Title");
+    expect(wrapper.findAll("h2")).toHaveLength(1);
+  });
+
+  it("should config component when provide all props", () => {
+    const wrapper = mount(ContainerHeader, {
+      props: { title: "Exemplo", titleIs: "h1", description: "Descrição" },
+    });
+
+    expect(wrapper.text()).toContain("Exemplo");
+    expect(wrapper.text()).toContain("Descrição");
+    expect(wrapper.findAll("h1")).toHaveLength(1);
+  });
 });
