78f9acd2d27fbe90e043bb6c4af33b454caeab65
Carlos Roberto <carlos.junior@dotgroup.com.br>
2023-04-03 22:47:31 -0300

    feat: add lang component to home

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -6,17 +6,24 @@ import BlockContainer from "@/components/BlockContainer";
 import HeaderContainer from "@/components/HeaderContainer";
 import TheCard from "@/components/TheCard";
 
-import projectsPtBr from "~/data/projects-pt-br.json";
-import projectsEn from "~/data/projects-en.json";
+import projectsPtBr from "@/data/projects-pt-br.json";
+import projectsEn from "@/data/projects-en.json";
 
-import TextContainer from "~/components/TextContainer";
+import TextContainer from "@/components/TextContainer";
+import { storageService } from "@/services";
+import LanguageSelector from "@/components/LanguageSelector";
 
-const { t, locale } = useI18n();
+const { t, locale, availableLocales } = useI18n();
+const lang = storageService.load("lang");
 
 const projects = computed(() => {
   if (locale.value == "pt-BR") return projectsPtBr;
   else return projectsEn;
 });
+
+if (lang && availableLocales.includes(lang)) {
+  locale.value = lang;
+}
 
 useHead({
   title: t("home.head.title"),
@@ -26,6 +33,9 @@ useHead({
 <template>
   <BlockContainer bg="mk-2t color" cover>
     <div class="m-20"></div>
+
+    <LanguageSelector />
+
     <HeaderContainer
       :title="$t('home.title')"
       :description="$t('home.description')"
