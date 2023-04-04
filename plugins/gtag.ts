45c0e42a33c6122c6206a672f8125e6bab9a4c0d
Carlos Roberto <carlos.junior@dotgroup.com.br>
2023-04-04 01:00:15 -0300

    fix: to correct GTAG undefined error

--- plugins/gtag.ts
+++ plugins/gtag.ts
@@ -3,5 +3,5 @@ import VueGtag from "vue-gtag-next";
 const GTAG = import.meta.env.VITE_GTAG?.toString();
 
 export default defineNuxtPlugin(({ vueApp }) => {
-  if (GTAG) vueApp.use(VueGtag, { property: { id: GTAG } });
+  if (GTAG) vueApp.use(VueGtag, { property: { id: GTAG ? GTAG : "" } });
 });
