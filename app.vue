890255e4327b30777839963a5731f29cb25acf94
Carlos Júnior <crpu4a@gmail.com>
2024-10-21 17:34:55 -0300

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,3 +1,11 @@
+<script lang="ts" setup>
+useHead({
+  meta: [
+    { name: "google-adsense-account", content: "ca-pub-5691189191143225" },
+  ],
+});
+</script>
+
 <template>
   <div id="app" class="max-w-full min-h-screen">
     <NuxtPage />
